0da47f7f17fc4b155137f273cf6361609e051086
LadySerena <serena.tiede+github@gmail.com>
2020-07-23 22:06:16 -0500

    feat: add basic auth middleware to api server
    require basic auth for all endpoints

--- cmd/dbus-api/main.go
+++ cmd/dbus-api/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"dbus-api/pkg/auth"
 	"dbus-api/pkg/dbus"
 	"dbus-api/pkg/server"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -15,8 +19,17 @@ func main() {
 		return
 	}
 	defer client.Close()
+	fmt.Println(os.Getwd())
+	db, dbErr := auth.NewDatabase("./secrets/decrypted/authUsers")
+	if dbErr != nil {
+		fmt.Printf("could not create database due to: %s\n", dbErr.Error())
+		return
+	}
 	config := server.NewConfig(client, "docker.service")
-	http.HandleFunc("/service", config.GetService)
-	http.HandleFunc("/meow/service", config.PostService)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter()
+
+	router.Use(db.BasicAuthMiddleware)
+	router.HandleFunc("/service", config.PostService).Methods(http.MethodPost)
+	router.HandleFunc("/service", config.GetService).Methods(http.MethodGet)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
