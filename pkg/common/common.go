5302a29db7b4351d5f70c404dbcd347d7d34656c
LadySerena <serena.tiede+github@gmail.com>
2020-07-23 21:34:01 -0500

    feat: add common problem json functions
    These functions use sprintf instead of json.marshal to avoid any errors
    with marshalling the json response.

---
+++ pkg/common/common.go
@@ -0,0 +1,11 @@
+package common
+
+import "fmt"
+
+func GenerateErrorResponse(code int32, text string, err error) []byte {
+	return []byte(fmt.Sprintf(`{"status": %d, "reason":"%s due to: %s"}`, code, text, err.Error()))
+}
+
+func GenerateProblemResponse(code int32, text string) []byte {
+	return []byte(fmt.Sprintf(`{"status": %d, "reason":"%s"}`, code, text))
+}
