c772e9597f8c85e8ebead26408ce8388140f9101
LadySerena <serena.tiede+github@gmail.com>
2021-08-10 23:22:10 -0500

    chore: remove unused constants

--- cmd/dbus-api/main.go
+++ cmd/dbus-api/main.go
@@ -5,18 +5,6 @@ import (
 	"log"
 )
 
-const (
-	prefix               = "dbus_api"
-	serviceKey           = "service_name"
-	authFileKey          = "auth_file"
-	listenAddressKey     = "listen_address"
-	listenerDefault      = "localhost:8080"
-	tlsEnabledKey        = "tls_enabled"
-	tlsEnabledDefault    = false
-	tlsCertPathKey       = "tls_cert_path"
-	tlsPrivateKeyPathKey = "tls_key_path"
-)
-
 func main() {
 	app, appErr := config.NewApp()
 	if appErr != nil {
